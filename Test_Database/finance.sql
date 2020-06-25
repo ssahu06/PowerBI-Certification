c3b3fa96a535f03394671a64d415b2a600e5cceb
Sunil Kumar Sahu <sunilkumar.sahu@actian.com>
2020-06-25 17:21:08 +0530

    Test Database used for Sample Pbix

---
+++ Test_Database/finance.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Finance
+(
+    Segment VARCHAR(50),
+    Country VARCHAR(50),
+    Product VARCHAR(50),
+    Discount_Band VARCHAR(10),
+    Units_Sold DOUBLE PRECISION,
+    Manufacturing_Price DOUBLE PRECISION,
+    Sale_Price DOUBLE PRECISION,
+    Gross_Sales DOUBLE PRECISION,
+    Discounts DOUBLE PRECISION,
+    Sales DOUBLE PRECISION,
+    COGS DOUBLE PRECISION,
+    Profit DOUBLE PRECISION,
+    Date DATE,
+    Month_Number INTEGER,
+    Month_Name VARCHAR(15),
+    Year VARCHAR(10)
+ );
